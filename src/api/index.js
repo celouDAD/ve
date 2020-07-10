181370a80fbcc0db5c47e28df1ff627b5206809c
celouDAD <511248665@qq.com>
2020-07-11 02:57:23 +0800

    管理后台更新

---
+++ src/api/index.js
@@ -0,0 +1,13 @@
+import * as api from './api'
+
+export default api
+
+// proxyTable:{
+//     '/': {
+//             target: 'http://localhost:8016/', //你要访问的服务器域名
+//             changeOrigin: true, //允许跨域
+//             pathRewrite: {
+//                 '^/': ''
+//             }
+//         }
+// }
