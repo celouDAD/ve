22c5dac33611a3fa45abddd88ad309dcbbba649c
celouDAD <511248665@qq.com>
2020-07-11 21:15:16 +0800

    vue test

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -34,12 +34,12 @@
         rules: {
           username: [{
             required: true,
-            message: '请输入账号',
+            message: '请输入账号!',
             trigger: 'blur'
           }],
           pwd: [{
             required: true,
-            message: '请输入密码',
+            message: '请输入密码!',
             trigger: 'blur'
           }]
         },
